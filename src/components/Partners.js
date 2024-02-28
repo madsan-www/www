f805db63d17b59a08a5d87731e2d911c66ca42a5
Emre <emre.bahar35@outlook.com>
2024-02-28 16:07:10 +0300

    Update Partners.js

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -29,7 +29,7 @@ function Partners() {
             >
               <img
                 src={logo.image}
-                className="avatar avatar-small "
+                className="avatar avatar-medium"
                 alt={logo.client}
               />
             </div>
