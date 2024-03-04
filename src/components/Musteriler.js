5b406ca3b33afd5eb1029907c5116f4f51daef55
Emre <emre.bahar35@outlook.com>
2024-03-04 17:07:03 +0300

    Create Musteriler.js

---
+++ src/components/Musteriler.js
@@ -0,0 +1,42 @@
+import * as React from "react";
+import Musteriler from "../../data/Musteriler.json";
+import { useLocale } from "../hooks/locale";
+
+function Musteriler() {
+
+  const { locale } = useLocale();
+  const isDefaultLang = locale === "tr" ? true : false;
+  const title = isDefaultLang ? Musteriler.content.title : Musteriler.content.titleEng;
+
+  return (
+    <>
+      <div className="container mt-100">
+        <div className="title-heading ms-lg-4 text-center">
+        <div className="row justify-content-center">
+            <div className="col-12">
+              <div className="section-title text-center mb-4 pb-2">
+                <h4 className="title mb-4 text-uppercase">{title}</h4>
+              </div>
+            </div>
+            {/*end col*/}
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {Musteriler.Logos.map((logo, index) => (
+            <div
+              key={`_${index}`}
+              className="col-lg-2 col-md-2 col-6 text-center py-4"
+            >
+              <img
+                src={logo.image}
+                className="avatar avatar-medium"
+                alt={logo.client}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+export default Musteriler;
